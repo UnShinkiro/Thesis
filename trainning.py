69c8366bb7d66c9d6042c9d57b0d6805c3312851
Tong Shen <TongShen2000@gmail.com>
2021-07-23 14:54:03 +1000

    modularized pre-processing and trainning

--- trainning.py
+++ trainning.py
@@ -1,68 +1,10 @@
-from scipy.io import wavfile
-import os
 import pickle
 import numpy as np
 import tensorflow as tf
-import random
 from tensorflow import keras
-from pre_process import form_input_data
 
-INT16_MAX = 32767
-SAMPLE_RATE = 16000
-FRAME_SIZE = int(SAMPLE_RATE * 0.025)
-NFFT = 512 
-NFILT = 40
-N_SPEAKER = 32
-pre_emphasis = 0.97
-
-spk_list = os.listdir("vox/vox1_dev_wav")
-utterance = {}
-emphasized_data = []
-validation_dataset = []
-validation_data = []
-validation_label = []
-train_data = []
-train_label = []
-enrollment_dataset = []
-verification_dataset = []
-
-# Text-independent Data processing
-for pid, speaker in enumerate(spk_list[0:N_SPEAKER]):
-    if not speaker.startswith("."):
-        utterance[speaker] = {}
-        path = "vox/vox1_dev_wav/" + speaker
-        folders = os.listdir(path)
-        utterance[speaker]['files'] = []
-        for folder in folders:
-            if not folder.startswith("."):
-                path = "vox/vox1_dev_wav/" + speaker + "/" + folder
-                files = os.listdir(path)
-                print(files)
-                for file in files:
-                    if not file.startswith("."):
-                        utterance[speaker]['files'].append(folder + "/" + file)
-
-        for count in range(10):
-            file_path = "vox/vox1_dev_wav/" + speaker + "/" + utterance[speaker]['files'].pop(0)
-            try:
-                _, data = wavfile.read(file_path)         # requires tons of memory with many spekaers
-                emphasized_signal = np.append(data[0], data[1:] - pre_emphasis * data[:-1])
-                if count < 5:
-                    emphasized_data.append((emphasized_signal,pid))
-                elif count < 10:
-                    validation_dataset.append((emphasized_signal,pid))
-            except:
-                pass
-
-for entry in emphasized_data:
-    form_input_data(entry, train_data, train_label)
-for entry in validation_dataset:
-    form_input_data(entry, validation_data, validation_label)
-
-with open('trainning_data.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
-    pickle.dump([train_data, train_label, validation_data, validation_label], f)
-with open('utterance_list.pkl', 'wb') as f:
-    pickle.dump([utterance, spk_list], f)
+with open('trainning_data.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
+    train_data, train_label, validation_data, validation_label = pickle.load(f)
 
 inputs = keras.layers.Input(shape=(NFILT*41,))
 dense1 = keras.layers.Dense(256, kernel_regularizer='l2', activation='relu')(inputs)
