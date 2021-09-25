41217d49fe5ce6000b3cafd910fbbb745181b8fc
Tong Shen <TongShen2000@gmail.com>
2021-09-25 16:42:02 +1000

    make 20 folders

--- job.sh
+++ job.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-python trainning.py 5
-python trainning.py 6
-python trainning.py 7
-python trainning.py 8
-python trainning.py 9
-python trainning.py 10
-python trainning.py 11
-python trainning.py 12
-python trainning.py 13
-python trainning.py 14
-python trainning.py 15
-python trainning.py 16
-python trainning.py 17
-python trainning.py 18
-python trainning.py 19
-cp -r results/ ../../../srv/scratch/z5195063/
+for i in {0..19}
+do
+   mkdir d-vector/i/
+done
