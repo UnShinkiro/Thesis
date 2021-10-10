1a8d26b7326544a9c42aa8d27303f60f2575b054
Tong Shen <TongShen2000@gmail.com>
2021-10-10 23:59:46 +1100

    experiment bash file

--- job.sh
+++ job.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-for i in {0..19}
+## declare an array variable
+declare -a arr=(0 0.1 0.2 0.3 0.4 0.5 0.6 0.7)
+
+## now loop through the above array
+for i in "${arr[@]}"
 do
-   mkdir d-vector/$i/
+   echo "$i"
 done
