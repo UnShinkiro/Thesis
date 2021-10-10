a0d624ce041b0262a46e9874abf56f7dffd3ba68
Tong Shen <TongShen2000@gmail.com>
2021-10-10 23:53:53 +1100

    modified function for reading result

--- read_results.py
+++ read_results.py
@@ -1,10 +1,12 @@
 import pickle
 import sys
 
-MODEL = int(sys.argv[1])
+MODEL = 0
+THRESHOLD = int(sys.argv[1])
 
-with open(f'results/{MODEL}.pkl', 'rb') as f:  # Python 3: open(..., 'wb')
-    correct_count, incorrect_count = pickle.load(f)
+with open(f'results/{THRESHOLD}.pkl', 'rb') as f:  # Python 3: open(..., 'wb')
+    same_correct,same_false,different_correct,different_false = pickle.load(f)
 
-print(f'Correct: {correct_count}, Incorrect: {incorrect_count}')
-print(f'Accuracy: {correct_count/(correct_count + incorrect_count)}')
+print(f'Total Correct: {same_correct + different_correct}, Total Incorrect: {same_false + different_false}')
+print(f'False Negative: {same_false/(80*40)}')
+print(f'False Positive: {different_false/(78*40)}')
