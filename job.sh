daba0357f8932b44d07a12a0aa86fe0079b01be2
Tong Shen <TongShen2000@gmail.com>
2021-10-11 03:26:43 +1100

    running uncertainty for multiple threshold

--- job.sh
+++ job.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 ## declare an array variable
-declare -a arr=(0 0.1 0.2 0.3 0.4 0.5 0.6 0.7)
+#declare -a arr=(0 0.1 0.2 0.3 0.4 0.5 0.6 0.7)
 
 ## now loop through the above array
-for i in "${arr[@]}"
-do
-   python evaluation.py "$i"
-done
+#for i in "${arr[@]}"
+#do
+#   python evaluation.py "$i"
+#done
+
+python uncertainty_for_one.py 0.5
+cp -r results/ ../../../../srv/scratch/z5195063/
