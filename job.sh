843c0c989ee509b6adc2a6777674d07e488d8fe8
Tong Shen <TongShen2000@gmail.com>
2021-10-18 03:11:10 +1100

    rerunning speaker enrollment

--- job.sh
+++ job.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-#cd ../../../srv/scratch/z5195063/
-#cp -r saved_model/ $TMPDIR
-#cp -r results/ $TMPDIR
-#cp -r d-vector/ $TMPDIR
-#cp test_utterance.pkl $TMPDIR
-#cp vox/vox1_test_wav.tar $TMPDIR
-#cd $TMPDIR
-#cp -r ~/Thesis/ .
-#tar xf vox1_test_wav.tar
-#mkdir Thesis/vox/
-#mv saved_model/ Thesis/
-#mv results/ Thesis/
-#mv d-vector/ Thesis/
-#mv test_utterance.pkl Thesis/
-#mv vox1_test_wav/ Thesis/vox/
-#cd Thesis
-#source ~/.venvs/Thesis/bin/activate
+cd ../../../srv/scratch/z5195063/
+cp -r saved_model/ $TMPDIR
+cp -r results/ $TMPDIR
+cp -r d-vector/ $TMPDIR
+cp test_utterance.pkl $TMPDIR
+cp vox/vox1_test_wav.tar $TMPDIR
+cd $TMPDIR
+cp -r ~/Thesis/ .
+tar xf vox1_test_wav.tar
+mkdir Thesis/vox/
+mv saved_model/ Thesis/
+mv results/ Thesis/
+mv d-vector/ Thesis/
+mv test_utterance.pkl Thesis/
+mv vox1_test_wav/ Thesis/vox/
+cd Thesis
+source ~/.venvs/Thesis/bin/activate
 
 ## declare an array variable
 declare -a arr=(25 20 15 10)
