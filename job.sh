61ef39788dee6892dc4e81e97c9aa061e9fdb794
Tong Shen <TongShen2000@gmail.com>
2021-10-24 21:46:13 +1100

    update job.sh

--- job.sh
+++ job.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 ## declare an array variable
-declare -a arr=(20 15 10)
+declare -a arr=(0.7 0.6 0.5 0.4 0.3 0.2)
+declare -a loop=(20 15 10)
 
 ## now loop through the above array
-for i in "${arr[@]}"
+for n in "${loop[@]}"
 do
-   python uncertainty_for_one.py "$i"
+echo "Uncertainty extraction for $n utterances model"
+   for i in "${arr[@]}"
+   do
+      python uncertainty_extraction.py "$i $n"
+   done
 done
 
 cp -r results/ ../../../../srv/scratch/z5195063/
