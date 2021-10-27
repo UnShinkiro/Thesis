c509579640688fa3216f4e808f667f5d3edaf831
Tong Shen <TongShen2000@gmail.com>
2021-10-27 21:55:02 +1100

    extracting uncertainties for less utterance enrolled models

--- job.sh
+++ job.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 ## declare an array variable
-#declare -a arr=(0.7 0.6 0.5 0.4 0.3 0.2)
+declare -a arr=(0.7 0.6 0.5 0.4 0.3 0.2)
 declare -a loop=(9 8 7 6 5)
 
 ## now loop through the above array
 for n in "${loop[@]}"
 do
 echo "extracting uncertainties from $n utterances enrolled speaker models"
-python uncertainty_for_one.py "$n"
-#   for i in "${arr[@]}"
-#   do
-#      python uncertainty_extraction.py "$i" "$n"
-#   done
+   for i in "${arr[@]}"
+   do
+      python uncertainty_extraction.py "$i" "$n"
+   done
 done
 
 cp -r results/ ../../../../srv/scratch/z5195063/
